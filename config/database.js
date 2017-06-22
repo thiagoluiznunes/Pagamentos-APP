5db5df6d6893411b546d6140666b6984a175c40c
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-06-22 02:36:37 -0300

    adding integration with mLab databse

--- config/database.js
+++ config/database.js
@@ -1,6 +1,13 @@
 //Responsável pela conexão e mapeamento com o MongoDB
 const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://localhost/db_finance')
+
+module.exports = function (user) {
+  const urlDev = 'mongodb://localhost/db_finance'
+  const urlProduction = 'mongodb://pagamentosapp:ti1606@ds133932.mlab.com:33932/db_finance'
+  const url = (user == 'dev' ? urlDev : urlProduction)
+
+  mongoose.connect(url)
+}
 
 mongoose.Error.messages.general.required = "0 atributo '{PATH}' é obrigatório"
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
--- loader.js
+++ loader.js
@@ -1,3 +1,3 @@
 const server = require('./config/server')
-require('./config/database')
+require('./config/database')('production')
 require('./config/routes')(server)
